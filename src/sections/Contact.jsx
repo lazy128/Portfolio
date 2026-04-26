73fc3c95f514614ee47ac5412d82697354a07ebc
lazy128 <duy.nguyenduy128@hcmut.edu.vn>
2026-04-26 15:13:27 +0700

    Updated

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -63,18 +63,19 @@ const Contact = () => {
 
     try {
       console.log("Form submitted:", formData);
-      await emailjs.send(
-        "service_79b0nyj",
-        "template_17us8im",
-        {
-          from_name: formData.name,
-          to_name: "Duy",
-          from_email: formData.email,
-          to_email: "duynguyen1282005@gmail.com",
-          message: formData.message,
-        },
-        "pn-Bw_mS1_QQdofuV"
-      );
+      // Trong file Contact.jsx
+    await emailjs.send(
+      import.meta.env.VITE_EMAILJS_SERVICE_ID,
+      import.meta.env.VITE_EMAILJS_TEMPLATE_ID,
+      {
+        from_name: formData.name,
+        to_name: "Duy",
+        from_email: formData.email,
+        to_email: "duynguyen1282005@gmail.com",
+        message: formData.message,
+      },
+  import.meta.env.VITE_EMAILJS_PUBLIC_KEY
+);
       setIsLoading(false);
       setFormData({ name: "", email: "", message: "" });
       setErrors({});
